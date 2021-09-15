ff3d88b0d6730f8938d94e3db8be3aabf82e31c6
Sc4ndium90 <b1jam1riot@gmail.com>
2021-09-15 21:27:44 +0200

    Try to fix context error n°1

--- src/main/kotlin/webhook/RequestHandler.kt
+++ src/main/kotlin/webhook/RequestHandler.kt
@@ -10,135 +10,134 @@ import webhook.server.ServerWebhookEvent
 import webhook.server.ServerWebhookListener
 import webhook.utils.RequestManager
 import webhook.utils.ResponseManager
+import java.util.*
+import java.util.function.Consumer
 
-class RequestHandler(private val listenerCollection: ListenerCollection): HttpHandler {
 
-    override fun handle(exchange: HttpExchange) {
-
+class RequestHandler(private val listenerCollection: ListenerCollection) : HttpHandler {
+    override fun handle(httpExchange: HttpExchange) {
         var currOptions: List<Options>? = null
         var validationDisabled = false
-
         try {
-
-            val reqManager = RequestManager(exchange)
-            val resManager = ResponseManager(exchange)
+            val reqManager = RequestManager(httpExchange)
+            val resManager = ResponseManager(httpExchange)
             val dataString = reqManager.string
-
             var authorization: String
             try {
-                authorization = exchange.requestHeaders.getFirst("Authorization")
-                if (authorization == null) authorization = "none"
+                authorization = httpExchange.requestHeaders.getFirst("Authorization")
+                if (authorization == null) {
+                    authorization = "none"
+                }
             } catch (ignored: Exception) {
                 authorization = "none"
             }
 
-            resManager.setResponseCode(200).writeResponse("Received :)")
-
+            // send earlier because everything needed was stored already
+            resManager.setResponseCode(200).writeResponse("Received! Thank you :)")
             val listenerObject = listenerCollection.listener
-
             if (listenerObject is BotWebhookListener) {
+                val listener = listenerObject
+                val event = BotWebhookEvent(dataString, listener, authorization!!, listenerCollection.authorization)
+                val optionsList = listOf(*listenerCollection.options)
 
-                val event = BotWebhookEvent(dataString, listenerObject, authorization, listenerCollection.authorization)
-                val optionsList = listenerCollection.options.asList()
                 currOptions = optionsList
 
                 if (optionsList.contains(Options.ONLY_LISTEN_FOR_TEST_VOTES)) {
-                    if (event.vote.type != VoteType.TEST && event.vote.type != VoteType.INVALID) return
-                }
-
-                if (event.isAuthorized && event.isValid) {
-                    listenerObject.onWebhookRequest(event)
-                    return
-                }
-
-                if (!event.isAuthorized) {
-                    if (optionsList.contains(Options.IGNORE_AUTHORIZATION)) {
-                        if (!event.isValid) {
-                            if (optionsList.contains(Options.IGNORE_VALIDATION)) {
-                                validationDisabled = true
-                                listenerObject.onWebhookRequest(event)
-                            }
-                        } else {
-                            listenerObject.onWebhookRequest(event)
-                        }
+                    if (event.vote.type != VoteType.TEST && event.vote.type != VoteType.INVALID) {
                         return
                     }
                 }
 
-                if (!event.isValid) {
-                    if (optionsList.contains(Options.IGNORE_VALIDATION)) {
-                        validationDisabled = true
-                        if (!event.isAuthorized) {
-                            if (optionsList.contains(Options.IGNORE_AUTHORIZATION)) {
-                                listenerObject.onWebhookRequest(event)
-                            }
-                        } else {
-                            listenerObject.onWebhookRequest(event)
-                        }
-                    }
-                }
-
-            } else if (listenerObject is ServerWebhookListener) {
-
-                val event =
-                    ServerWebhookEvent(dataString, listenerObject, authorization, listenerCollection.authorization)
-                val optionsList = listenerCollection.options.toList()
-                currOptions = optionsList
-
-                if (optionsList.contains(Options.ONLY_LISTEN_FOR_TEST_VOTES)) {
-                    if (event.vote.type != VoteType.TEST && event.vote.type != VoteType.INVALID) return
-                }
-
                 if (event.isAuthorized && event.isValid) {
-                    listenerObject.onWebhookRequest(event)
+                    listener.onWebhookRequest(event)
                     return
                 }
-
                 if (!event.isAuthorized) {
                     if (optionsList.contains(Options.IGNORE_AUTHORIZATION)) {
                         if (!event.isValid) {
                             if (optionsList.contains(Options.IGNORE_VALIDATION)) {
                                 validationDisabled = true
-                                listenerObject.onWebhookRequest(event)
+                                listener.onWebhookRequest(event)
                             }
                         } else {
-                            listenerObject.onWebhookRequest(event)
+                            listener.onWebhookRequest(event)
                         }
+                        return
                     }
                 }
-
                 if (!event.isValid) {
                     if (optionsList.contains(Options.IGNORE_VALIDATION)) {
                         validationDisabled = true
                         if (!event.isAuthorized) {
                             if (optionsList.contains(Options.IGNORE_AUTHORIZATION)) {
-                                listenerObject.onWebhookRequest(event)
+                                listener.onWebhookRequest(event)
                             }
                         } else {
-                            listenerObject.onWebhookRequest(event)
+                            listener.onWebhookRequest(event)
+                        }
+                    }
+                }
+            } else if (listenerObject is ServerWebhookListener) {
+                val listener: ServerWebhookListener = listenerObject
+                val event = ServerWebhookEvent(dataString, listener, authorization, listenerCollection.authorization)
+                val optionsList = Arrays.asList(*listenerCollection.options)
+
+                // Exception purposes
+                currOptions = optionsList
+                if (optionsList.contains(Options.ONLY_LISTEN_FOR_TEST_VOTES)) {
+                    if (!event.vote.type.equals(VoteType.TEST) && !event.vote.type
+                            .equals(VoteType.INVALID)
+                    ) {
+                        return
+                    }
+                }
+
+                //TODO please make this somewhat more efficient
+                if (event.isAuthorized && event.isValid) {
+                    listener.onWebhookRequest(event)
+                    return
+                }
+                if (!event.isAuthorized) {
+                    if (optionsList.contains(Options.IGNORE_AUTHORIZATION)) {
+                        if (!event.isValid) {
+                            if (optionsList.contains(Options.IGNORE_VALIDATION)) {
+                                validationDisabled = true
+                                listener.onWebhookRequest(event)
+                            }
+                        } else {
+                            listener.onWebhookRequest(event)
+                        }
+                        return
+                    }
+                }
+                if (!event.isValid) {
+                    if (optionsList.contains(Options.IGNORE_VALIDATION)) {
+                        validationDisabled = true
+                        if (!event.isAuthorized) {
+                            if (optionsList.contains(Options.IGNORE_AUTHORIZATION)) {
+                                listener.onWebhookRequest(event)
+                            }
+                        } else {
+                            listener.onWebhookRequest(event)
                         }
                     }
                 }
             } else {
                 println("Error handling Request")
             }
-
         } catch (e: Exception) {
             System.err.println("Error while handling request")
             if (validationDisabled) {
                 System.err.println("Note: Validation was disabled through the Listener, check first before asking for help!")
                 System.err.println("Referring Option: IGNORE_VALIDATION")
             }
-            System.err.println("Active Options")
-
+            System.err.print("Active Options: ")
             assert(currOptions != null)
-            currOptions!!.forEach { options -> System.err.println(options.name + ", ") }
+            currOptions!!.forEach(Consumer { options: Options ->
+                System.err.print(options.name + ", ")
+            })
             System.err.println()
             e.printStackTrace()
         }
-
-
     }
-
-
 }
